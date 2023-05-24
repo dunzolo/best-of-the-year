f1e2f8a49e5b1160ae9ef784e070b1df08887563
dunzolo <rossidavide108@gmail.com>
2023-05-24 15:02:26 +0200

    first commit

---
+++ src/test/java/org/lesson/java/proj/ProjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lesson.java.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
