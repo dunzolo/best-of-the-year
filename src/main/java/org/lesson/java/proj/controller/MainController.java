a936812dc80c14df36f37f57e51c21f6d1e783ea
dunzolo <rossidavide108@gmail.com>
2023-05-25 17:26:24 +0200

    step 2 done

--- src/main/java/org/lesson/java/proj/controller/MainController.java
+++ src/main/java/org/lesson/java/proj/controller/MainController.java
@@ -16,81 +16,105 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
 	
 	@GetMapping("/")
-	public String printFirstString(Model model,
-			@RequestParam(name = "name") String name) {
-		model.addAttribute("name", name);
+	public String getHome(Model model) {
+		
+		model.addAttribute("name", "Davide");
 		
 		return "index";
 	}
 	
 	@GetMapping("/movies")
 	public String getMoviesList(Model model) {
-		String movie_list = "";
+		//String movie_list = "";
 		
 		//for(Movie movie : getBestMovies()) {
 		//	movie_list += movie.getTitolo() + ", ";
 		//}
 		//movie_list = movie_list.substring(0, movie_list.length() - 1);
 		
-		Iterator<Movie> it = getBestMovies().iterator();
-		while(it.hasNext()) {
-			movie_list += it.next().getTitolo();
-			if(it.hasNext()) movie_list += ", ";
-		}
+		//Iterator<Movie> it = getBestMovies().iterator();
+		//while(it.hasNext()) {
+		//	movie_list += it.next().getTitolo();
+		//	if(it.hasNext()) movie_list += ", ";
+		//}
 		
-		model.addAttribute("name", movie_list);
+		List<Movie> movies = getBestMovies();
+
+		model.addAttribute("movies", movies);
 		
-		return "index";
+		return "movies";
 	}
 	
 	@GetMapping("/movies/{id}")
-	public String getMovieId(Model model,
+	public String getMovie(Model model,
 			@PathVariable("id") int id) {
 		
-		String movie_id = null;
-		for( Movie movie : getBestMovies()) {
-			if(movie.getId() == id) {				
-				movie_id = movie.getTitolo(); 
-			}
-		}
+//		String movie_id = null;
+//		for( Movie movie : getBestMovies()) {
+//			if(movie.getId() == id) {				
+//				movie_id = movie.getTitolo(); 
+//			}
+//		}
+//		
+//		if(movie_id != null) model.addAttribute("name", movie_id);
+//		else model.addAttribute("name", "movie not found");
+//		
+//		return "index";
 		
-		if(movie_id != null) model.addAttribute("name", movie_id);
-		else model.addAttribute("name", "movie not found");
+		Movie resM = null;
+		for (Movie m : getBestMovies()) 
+			if (m.getId() == id)
+				resM = m;
 		
-		return "index";
+		if (resM != null) 
+			model.addAttribute("movie", resM);
+		
+		
+		return "movie-detail";
 	}
 	
 	@GetMapping("/songs")
 	public String getSongsList(Model model) {
-		String song_list = "";
+//		String song_list = "";
+//		
+//		Iterator<Song> it = getBestSong().iterator();
+//		while(it.hasNext()) {
+//			song_list += it.next().getTitolo();
+//			if(it.hasNext()) song_list += ", ";
+//		}
+//		
+//		model.addAttribute("name", song_list);
+//		
+//		return "songs";
 		
-		Iterator<Song> it = getBestSong().iterator();
-		while(it.hasNext()) {
-			song_list += it.next().getTitolo();
-			if(it.hasNext()) song_list += ", ";
-		}
+		List<Song> songs = getBestSong();
+
+		model.addAttribute("songs", songs);
 		
-		model.addAttribute("name", song_list);
-		
-		return "index";
+		return "songs";
 	}
 	
 	@GetMapping("/songs/{id}")
-	public String getSongId(Model model,
+	public String getSong(Model model,
 			@PathVariable("id") int id) {
 		
-		String song_id = null;
-		for( Song song : getBestSong()) {
-			if(song.getId() == id) {				
-				song_id = song.getTitolo(); 
-			}
-		}
+//		String song_id = null;
+//		for( Song song : getBestSong()) {
+//			if(song.getId() == id) {				
+//				song_id = song.getTitolo(); 
+//			}
+//		}
 		
-		if(song_id != null) model.addAttribute("name", song_id);
-		else model.addAttribute("name", "song not found");
+		Song resS = null;
+		for (Song s : getBestSong()) 
+			if (s.getId() == id)
+				resS = s;
+		
+		if (resS != null) 
+			model.addAttribute("song", resS);
 		
 		
-		return "index";
+		return "song-detail";
 	}
 	
 	//methods
